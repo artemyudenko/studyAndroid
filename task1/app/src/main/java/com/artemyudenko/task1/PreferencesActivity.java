ee19301484e3f01a17795769a280a3984233bae4
Artem Yudenko <s12676@pjwstk.edu.pl>
2018-10-21 21:44:45 +0200

    Preferences are ready, + toast

--- task1/app/src/main/java/com/artemyudenko/task1/PreferencesActivity.java
+++ task1/app/src/main/java/com/artemyudenko/task1/PreferencesActivity.java
@@ -2,14 +2,18 @@ package com.artemyudenko.task1;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.artemyudenko.task1.constants.Constants;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class PreferencesActivity extends AppCompatActivity {
 
@@ -22,7 +26,16 @@ public class PreferencesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
         sharedPreferences = getPreferences(Context.MODE_PRIVATE);
-        fillInDropdown();
+        editText = findViewById(R.id.editText);
+
+        String defaulSavedTextPrefs = "Size";
+        String savedTextPref = sharedPreferences.getString(Constants.PREFERENCES_VALUE_KEY.getKey(),
+                defaulSavedTextPrefs);
+        editText.setText(savedTextPref);
+
+        String savedDropDown = sharedPreferences.getString(Constants.PREFERENCES_VALUE_KEY.getKey(),
+                "");
+        fillInDropdown(savedDropDown);
     }
 
     public void onSaveClick(View view) {
@@ -36,12 +49,18 @@ public class PreferencesActivity extends AppCompatActivity {
         editor.putString(Constants.PREFERENCE_COLOR_KEY.getKey(), selectedColor);
 
         editor.apply();
+
+        Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
     }
 
-    private void fillInDropdown() {
-        String[] colors = {"red", "blue", "green"};
+    private void fillInDropdown(String savedDropDown) {
+        List colors = Arrays.asList( "Red", "Blue", "Green");
         dropdown = findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, colors);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, (String [])colors.toArray());
         dropdown.setAdapter(adapter);
+        if (!savedDropDown.isEmpty()) {
+            int i = colors.indexOf(savedDropDown);
+            dropdown.setSelection(i);
+        }
     }
 }
